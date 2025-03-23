cfb474f4eb184962818eab6b8a3788ac68e61ddd
Govind <govindsys1008@gmail.com>
2025-03-23 13:46:38 +0530

    .

--- SkillHive/mySkill/urls.py
+++ SkillHive/mySkill/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', home, name='home'),  # Home page (lists courses)
     path('event/<slug:slug>/', event_detail, name='event_detail'),
      path('events/', event_list, name='event_list'), # Individual course detail page
-     path('contact_us', contact_us, name='contact_us'),
+     path('contact_us/', contact_us, name='contact_us'),
 ]
